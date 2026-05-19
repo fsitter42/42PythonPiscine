558e680be554ceb179b0f0229267db0d7a8902c1
fsitter42 <fsitter@student.42vienna.com>
2026-05-19 16:52:49 +0200

    suwi

--- module7/ex03/append_it.py
+++ module7/ex03/append_it.py
@@ -5,15 +5,10 @@ import sys
 ac = len(sys.argv)
 av = sys.argv
 
-if ac != 2:
+if ac < 2:
     print("none")
     sys.exit()
-count = av[1].find("ism")
-print(count)
-
-    
-
-
-
-
-    
+for i in range(1, ac):
+    pos = av[i].find("ism")
+    if pos == -1:
+        print(f"{av[i]}ism")
