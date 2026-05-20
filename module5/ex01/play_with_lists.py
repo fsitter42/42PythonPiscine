5e07fea149fe55cc82c4cf45e38c16d1dd75aaaf
fsitter42 <fsitter@student.42vienna.com>
2026-05-20 15:07:33 +0200

    suwi

--- module5/ex01/play_with_lists.py
+++ module5/ex01/play_with_lists.py
@@ -2,8 +2,9 @@
 
 a = [2, 8, 9, 48, 8, 22, -12, 2]
 b = []
+# b = [b+2 for b in a]
 for i in range(len(a)):
-    b.append(a[i] + 2) 
+    b.append(a[i] + 2)
 
 print("Original List: ", a)
 print("New List: ", b)
--- module8/ex03/greetings_for_all.py
+++ module8/ex03/greetings_for_all.py
@@ -1,2 +1,8 @@
 #!/bin/python3
 
+def greetings(s):
+    if s.is_string() == -1:
+        print("Error! It was not a name.")
+
+
+greetings(42)
