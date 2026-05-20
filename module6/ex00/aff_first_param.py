01f8a29ae62cff22d4665b1fd68c5cb00ee85bec
fsitter42 <fsitter@student.42vienna.com>
2026-05-20 14:14:54 +0200

    suwi

--- module6/ex00/aff_first_param.py
+++ module6/ex00/aff_first_param.py
@@ -4,3 +4,5 @@ import sys
 
 if len(sys.argv) > 1:
     print(sys.argv[1])
+else:
+    print("none")
--- module7/ex04/free_range.py
+++ module7/ex04/free_range.py
@@ -1,1 +1,17 @@
 #!/bin/python3
+
+import sys
+# import numpy as np
+
+ac = len(sys.argv)
+av = sys.argv
+arr = []
+
+if ac != 3:
+    print("none")
+    sys.exit()
+brr = list(range(int(av[1]), int(av[2]) + 1))
+print(brr)
+
+# arr = np.arange(int(av[1]), int(av[2]) + 1)
+# print(arr)
