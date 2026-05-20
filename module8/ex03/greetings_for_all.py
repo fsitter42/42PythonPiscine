94108976dbe3aff50ae4e50c6efb163bd39fa408
fsitter42 <fsitter@student.42vienna.com>
2026-05-20 15:13:07 +0200

    suwi

--- module8/ex03/greetings_for_all.py
+++ module8/ex03/greetings_for_all.py
@@ -1,8 +1,12 @@
 #!/bin/python3
 
-def greetings(s):
-    if s.is_string() == -1:
+def greetings(s="noble stranger"):
+    if isinstance(s, int) == 1:
         print("Error! It was not a name.")
+    else:
+        print(f"Hello, {s}.")
 
-
+greetings("Alexandra")
+greetings("Wil")
+greetings()
 greetings(42)
