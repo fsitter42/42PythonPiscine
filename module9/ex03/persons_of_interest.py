09b24f07490b832b676435f1045c54380447809d
fs42 <fsitter@student.42vienna.com>
2026-05-22 00:04:46 +0200

    suwi

---
+++ module9/ex03/persons_of_interest.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+def famous_births(dict):
+    for i in dict:
+        print(f" {dict[i]['name']} is a great scientist {dict[i]['date_of_birth']}.")
+
+women_scientists = {
+    "ada": { "name": "Ada Lovelace", "date_of_birth": "1815" },
+    "cecilia": { "name": "Cecila Payne", "date_of_birth": "1900" },
+    "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
+    "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
+}
+
+famous_births(women_scientists)
